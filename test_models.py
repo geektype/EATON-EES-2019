7b45b7c0da875eb3ab5cee4b59bb4d53bcf42674
Abdullah Ahmed <abdullah@softcomnetworks.com>
2020-02-15 14:14:41 +0000

    Modified test

--- test_models.py
+++ test_models.py
@@ -1,39 +1,13 @@
 import unittest
-from models import create_connection, storeReadings
+from models import storeReadings
 import datetime
+import requests
 
 class TestDatabase(unittest.TestCase):
-    def setUp(self):
-        try:
-            self.db_conn, self.cursor = create_connection("remotemysql.com", "VUqnoHhiHe", "DyOeYyDKHw", "VUqnoHhiHe")
-        
-        except Exception as e:
-            print(e)
-            self.fail("Failed to connect to test database")
-    # def test_test_database_empty(self):
-    #     self.cursor.execute("SELECT * FROM test;")
-        
-    #     self.result = []
-    #     for row in self.cursor:
-    #         self.result.append(row)
-    #     self.assertEquals(self.result, [], "test database should be empty")
-
     def test_insert_reading(self):
-        self.time = '2019-01-01 00:00:00'
-        self.reading = [['ldr1', '434'], ['ldr2', '361']]
-        storeReadings(self.reading, self.cursor, time=self.time)
-        self.db_conn.commit()
-
-
-        self.cursor.execute("SELECT name, value, time FROM test")
-        self.result = []
-        for row in self.cursor:
-            self.result.append(row)
-
-        self.expected = [('ldr1', 434.0, datetime.datetime(2019, 1, 1, 0, 0)), ('ldr2', 361.0, datetime.datetime(2019, 1, 1, 0, 0))]
-        self.cursor.execute("DELETE FROM test WHERE UID > 1")
-        self.db_conn.commit()
-        self.assertEquals(self.result, self.expected, "wrong or no entry")
+        url = "https://oq3xvtpr80.execute-api.eu-west-2.amazonaws.com/ees/store-reading"
+        req = requests.post(url, json={"sen_var": "test_var", "value":1.234})
+        self.assertEqual(req.status_code, 200, "Error storing reading")
 
         
 
